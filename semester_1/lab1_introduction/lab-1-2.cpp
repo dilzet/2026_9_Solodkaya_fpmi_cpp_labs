8133db5feeec7274afb6fc4b94ce0a7243afe1b6
dilzet <dilzet@mail.ru>
2026-09-10 01:31:08 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/lab-1-2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+//Вычислить сумму первых n нечётных чисел.Количество чисел N вводится с клавиатуры.
+int main()
+{
+	int n;
+	int sum = 0;
+
+	std::cout << "Enter positive n:" << std::endl;
+	std::cin >> n;
+
+	if (n <= 0) {
+		std::cout << "Error. Enter different number" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		sum += 2 * i - 1;
+	}
+
+	std::cout << "The sum of the N odd numbers =" << sum << std::endl;
+
+	return 0;
+}
+
+
---
+++ semester_1/lab1_introduction/lab_1-3.cpp
@@ -0,0 +1,31 @@
+﻿//С клавиатуры вводится четырёхзначное натуральное число.
+// Выяснить, является ли оно палиндромом
+// (т.е.читается одинаково как слева направо, так и справа налево).
+
+#include <iostream>
+
+int main() {
+    int number;
+
+    std::cout << "Enter a four-digit number: ";
+    std::cin >> number;
+
+    if (number < 1000 || number > 9999) {
+        std::cout << "Error: enter a four-digit number." << std::endl;
+        return 0;
+    }
+
+    int a = number / 1000;
+    int b = (number / 100) % 10;
+    int c = (number / 10) % 10;
+    int d = number % 10;
+
+    if (a == d && b == c) {
+        std::cout << "This number is a palidrome." << std::endl;
+    }
+    else {
+        std::cout << "This number is not a palidrome." << std::endl;
+    }
+
+    return 0;
+}
