1beaee676215c700555385052826e0a5dc607dfc
dilzet <dilzet@mail.ru>
2026-09-10 01:28:54 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/lab-1-5.cpp
@@ -0,0 +1,29 @@
+﻿//Напечатать первые n чисел Фибоначчи. Число n вводится с клавиатуры.
+// Числа Фибоначчи вычисляются по формуле : Fn= Fn−1 + Fn−2, где F0 = 0,F1 = 1.
+
+#include <iostream>
+
+int main()
+{
+	int n;
+
+	std::cout << "Enter n:" << std::endl;
+	std::cin >> n;
+	if (n <= 0) {
+		std::cout << "n must be positive" << std::endl;
+		return 0;
+	}
+	int F0 = 0;
+	int F1 = 1;
+		for (int i = 0; i < n; i++) {
+			std::cout << F0;
+			if (i < n - 1) {
+				std::cout << ' ';
+			}
+			int next = F0 + F1;
+			F0 = F1;
+			F1 = next;
+		}
+
+	return 0;
+}
---
+++ semester_1/lab1_introduction/lab-1-6.cpp
@@ -0,0 +1,30 @@
+﻿// Дана арифметическая прогрессия :ak = ak−1 + d.
+// Вывести все члены этой прогрессии, кратные 3, которые лежат на отрезке [a,b].
+// Числа a,b,d вводятся с клавиатуры (a0 = a).
+
+#include <iostream>
+
+int main()
+{
+    int a, d, b;
+    std::cout << "Enter a, d, b:" << std::endl;
+    std::cin >> a >> d >> b;
+
+    if ((a > b) || (b < a)) {
+        std::cout << "Error. Enter different a, b, d:" << std::endl;
+        return 0;
+    }
+
+    std::cout << "\nMultiple of 3:\n";
+
+    for (int ak = a; ak <= b; ak += d) {
+      
+        if (ak % 3 == 0) {
+            std::cout << ak << " ";
+        }
+
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
